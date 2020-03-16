537bb59ebdaa24b66603d7f57df469e9606e3162
alxgmv <alxgmv@gmail.com>
2020-03-16 15:27:28 +0100

    2nd

---
+++ data.js
@@ -0,0 +1,85 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-4b",
+      "name": "4b",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.10757628668554631,
+        "pitch": 0.25791524876872884,
+        "fov": 0.9102706502487995
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.5416411448957348,
+          "pitch": 0.3762040213729616,
+          "title": "Bed",
+          "text": "Zanotti"
+        },
+        {
+          "yaw": 0.15577972687048813,
+          "pitch": 0.3754874007145208,
+          "title": "Chair",
+          "text": "Vitra"
+        },
+        {
+          "yaw": -0.09890604788800417,
+          "pitch": 0.21501030715056402,
+          "title": "Table",
+          "text": "Zampa"
+        },
+        {
+          "yaw": -2.3326563221856613,
+          "pitch": 0.22087734713135632,
+          "title": "Sideboard",
+          "text": "Leaf"
+        },
+        {
+          "yaw": 1.5852679321965724,
+          "pitch": 0.23122315870679522,
+          "title": "Bookshelf",
+          "text": "Ikea"
+        },
+        {
+          "yaw": 0.8908196509766722,
+          "pitch": 0.39811633106794986,
+          "title": "Armchair",
+          "text": "Muuto"
+        },
+        {
+          "yaw": 0.7202281041559395,
+          "pitch": -0.04539921265181057,
+          "title": "Bike",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

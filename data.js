dd6f87729ad827813f57148a42e681f84d3ece70
alxgmv <alxgmv@gmail.com>
2020-03-19 15:52:22 +0100

    4th

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-123",
-      "name": "123",
+      "id": "0-paris-showroom",
+      "name": "Paris showroom",
       "levels": [
         {
           "tileSize": 256,
@@ -16,65 +16,87 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 1000,
+      "faceSize": 1500,
       "initialViewParameters": {
-        "yaw": 0.5872580439396113,
-        "pitch": 0.006367854178673582,
-        "fov": 1.2480461059535137
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": [
         {
-          "yaw": 1.3844605685522637,
-          "pitch": 0.43293620679473577,
-          "title": "Sofa Tyme",
-          "text": "<a href=\"https://de.mycs.com/produkt/6lQ5kJQbT\">https://de.mycs.com/produkt/6lQ5kJQbT</a>"
+          "yaw": 3.069858614703474,
+          "pitch": -0.06471581605088161,
+          "title": "GRYD etagere",
+          "text": "<a href=\"https://fr.mycs.com/etagere/rs4u1BEqgh?tab=verification\">fr.mycs.com/etagere/rs4u1BEqgh?tab=verification</a>"
         },
         {
-          "yaw": 0.6346340962545796,
-          "pitch": 0.7510003928867839,
-          "title": "Eclypse table",
-          "text": "<a href=\"https://de.mycs.com/produkt/2-6MxoC_c\">https://de.mycs.com/produkt/2-6MxoC_c</a>"
+          "yaw": 2.491205938905451,
+          "pitch": -0.12412317054399225,
+          "title": "chaise-CHAYR",
+          "text": "<a href=\"https://fr.mycs.com/chaise-chayr/ck0a2AZVR?tab=verification\">fr.mycs.com/chaise-chayr/ck0a2AZVR?tab=verification</a>"
         },
         {
-          "yaw": -0.19589368262062834,
-          "pitch": 0.1962515144857928,
-          "title": "Tymber table",
-          "text": "<a href=\"https://de.mycs.com/produkt/L-fWR0PQ\">https://de.mycs.com/produkt/L-fWR0PQ</a>"
+          "yaw": 2.3027792019152056,
+          "pitch": 0.015258082666147033,
+          "title": "chaise-CHAYR",
+          "text": "<a href=\"https://fr.mycs.com/chaise-chayr/Fi66HIIHV?tab=verification\">fr.mycs.com/chaise-chayr/Fi66HIIHV?tab=verification</a>"
         },
         {
-          "yaw": -1.597440958159499,
-          "pitch": 0.5158454508070349,
-          "title": "Gryd shelf",
-          "text": "<a href=\"https://de.mycs.com/produkt/H4MFSI5qX\">https://de.mycs.com/produkt/H4MFSI5qX</a>"
+          "yaw": 1.8257635921126045,
+          "pitch": 0.06224727515276385,
+          "title": "GRYD etagere",
+          "text": "<a href=\"https://fr.mycs.com/etagere/L1y-GIg27?tab=verification\">fr.mycs.com/etagere/L1y-GIg27?tab=verification</a>"
         },
         {
-          "yaw": 2.329043528880314,
-          "pitch": 0.25056570960539837,
-          "title": "Fyne cushion",
-          "text": "<a href=\"https://de.mycs.com/produkt/T1HVnV7Qf\">https://de.mycs.com/produkt/T1HVnV7Qf</a>"
+          "yaw": 1.8393910051493911,
+          "pitch": 0.5614360464954746,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">canape-PYLLOW</span>",
+          "text": "<a href=\"https://fr.mycs.com/canape-pyllow/42cmuZ_ZY?tab=verification\">fr.mycs.com/canape-pyllow/42cmuZ_ZY?tab=verification</a>"
         },
         {
-          "yaw": 1.9402380812795892,
-          "pitch": 0.21433174902351126,
-          "title": "Fyne cushion",
-          "text": "<a href=\"https://de.mycs.com/produkt/wfEi4Vh00\">https://de.mycs.com/produkt/wfEi4Vh00</a>"
+          "yaw": 0.9149725150128454,
+          "pitch": 0.8587220881445887,
+          "title": "bout-de-canape SYDE",
+          "text": "<a href=\"https://fr.mycs.com/bout-de-canape/F-X4sylAj?tab=verification\">fr.mycs.com/bout-de-canape/F-X4sylAj?tab=verification</a>"
         },
         {
-          "yaw": 0.579775732200309,
-          "pitch": 0.2690238535844536,
-          "title": "Fyne cushion",
-          "text": "<a href=\"https://de.mycs.com/produkt/T1HVnV7Qf\">https://de.mycs.com/produkt/T1HVnV7Qf</a>"
+          "yaw": 0.36685680972765944,
+          "pitch": 0.26561757450224555,
+          "title": "canape-TYME",
+          "text": "<a href=\"https://fr.mycs.com/canape-tyme/rVXW6qRUb?tab=verification\">fr.mycs.com/canape-tyme/rVXW6qRUb?tab=verification</a>"
+        },
+        {
+          "yaw": 1.2227098796680167,
+          "pitch": 0.358270212902692,
+          "title": "bout-de-canape SYDE",
+          "text": "<a href=\"https://fr.mycs.com/bout-de-canape/zHQbiPs0X?tab=verification\">fr.mycs.com/bout-de-canape/zHQbiPs0X?tab=verification</a>"
+        },
+        {
+          "yaw": -1.5366332177669975,
+          "pitch": 0.529432740497537,
+          "title": "canape-TYME",
+          "text": "<a href=\"https://fr.mycs.com/canape-tyme/waFtxHAZk?tab=verification\">fr.mycs.com/canape-tyme/waFtxHAZk?tab=verification</a>"
+        },
+        {
+          "yaw": -1.0402265288861337,
+          "pitch": 0.3546930742547012,
+          "title": "bout-de-canape SYDE",
+          "text": "<a href=\"https://fr.mycs.com/bout-de-canape/ziWipbYAv?tab=verification\">fr.mycs.com/bout-de-canape/ziWipbYAv?tab=verification</a>"
         }
       ]
     }
   ],
-  "name": "Interior sample",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
+    "autorotateEnabled": false,
     "fullscreenButton": false,
     "viewControlButtons": false
   }
